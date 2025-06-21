490f24dbdd259e87c207e36b49b9ebb61a12f3c0
Krishnavineeth <krishnavineethaddanki@gmail.com>
2025-06-21 19:15:20 +0530

    hello world started

--- variable.sh
+++ variable.sh
@@ -2,7 +2,7 @@
 PERSON1=Mom
 PERSON2=Son
 
-echo "$PERSON1: Hi How are you?"
-echo "$PERSON2: Hi Mom I'm fyn here"
+echo "$PERSON1: Hi $PERSON2 How are you?"
+echo "$PERSON2: Hi $PERSON1 I'm fyn here"
 echo "$PERSON1: Ok Take care"
 echo "$PERSON2:Sure You too"
