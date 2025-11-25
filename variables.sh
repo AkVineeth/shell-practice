a054bf8c6846e682ea52408752bec308ad3731f4
Krishnavineeth <krishnavineethaddanki@gmail.com>
2025-11-25 23:22:09 +0530

    hello world created

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1= Trump
+PERSON2= Musk
+
+echo "$PERSON1: Hey $PERSON2!"
+echo "$PERSON2: Hey $PERSON1!"
+echo "$PERSON1: Hey $PERSON2 lets emjoy"
+echo "$PERSON2: Hey $PERSON1 ok"
